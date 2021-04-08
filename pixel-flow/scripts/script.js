d5ef264ea462e2c98197cc5053e8f3afa90c6eef
Stefan <ankersostefan@gmail.com>
2021-04-08 20:05:50 +0200

    First commit

---
+++ pixel-flow/scripts/script.js
@@ -0,0 +1,111 @@
+const myImage = new Image();
+
+myImage.src = 'images/image1.png';
+
+myImage.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1080;
+    canvas.height = 1920;
+
+    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+    const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let particlesArray = [];
+    const numberOfParticles = 10000;
+
+    let mappedImage = [];
+    for (let y = 0; y < canvas.height; y++) {
+        let row = [];
+        for (let x = 0; x < canvas.width; x++) {
+            const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+            const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+            const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+            const brightness = calculateRelativeBrightness(red, green, blue);
+            const cell = [
+                cellBrightness = brightness,
+                cellColor = 'rgb(' + red + ',' + green + ',' + blue + ')'
+            ];
+            row.push(cell);
+        }
+        mappedImage.push(row);
+    }
+
+    function calculateRelativeBrightness(red, green, blue) {
+        return Math.sqrt(
+            (red * red) * 0.299 +
+            (green * green) * 0.587 +
+            (blue * blue) * 0.114
+        ) / 100;
+    }
+
+    //#region "class Particle"
+    class Particle {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+            this.speed = 0;
+            this.velocity = Math.random() * 3.5;
+            this.size = Math.random() * 1.5 + 1;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
+            this.angle = 0;
+        }
+        update() {
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
+
+            if ((mappedImage[this.position1]) && (mappedImage[this.position1][this.position2])) {
+                this.speed = mappedImage[this.position1][this.position2][0];
+            }
+
+            let movement = (2.5 - this.speed) + this.velocity;
+            this.angle += 0.2;
+
+            this.y += movement;
+            this.x += movement;
+            if (this.y >= canvas.height) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+            if (this.x >= canvas.width) {
+                this.x = 0;
+                this.x = Math.random() * canvas.height;
+            }
+        }
+        draw() {
+            ctx.beginPath();
+            if ((mappedImage[this.position1]) && (mappedImage[this.position1][this.position2])) {
+                ctx.fillStyle = mappedImage[this.position1][this.position2][1];
+            }
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+    //#endregion
+
+    function init() {
+        for (let i = 0; i < numberOfParticles; i++) {
+            particlesArray.push(new Particle);
+        }
+    }
+    init();
+
+    function animate() {
+        ctx.globalAlpha = 0.05;
+
+        ctx.fillStyle = mappedImage[this.position1][this.position2][1];;
+
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 0.2;
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            ctx.globalAlpha = particlesArray[i].speed * 0.1;
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
+
+});
